f4d36e57b4af743933fba8257d7f6bb65b697a9c
shaheenyoussif12345-beep <shaheenyoussif12345@gmail.com>
2025-11-23 18:35:09 +0200

    Add Telegram bot welcome message handler
    telegram welcome message

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 
+import telebot
+
+BOT_TOKEN = "8267938733:AAGhhKLDNx12UCNyGL6cn0I0eSQr_AINCs0"
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(
+        message.chat.id,
+        """  
+جميع التفاصيل اللي محتاجها داخل الجروب  
+لا تنسي متابعة الجروب الرسمي  
+ولا تتردد في التواصل مع الدعم عند وجود استفسار  
+
+https://t.me/+wnLokF1pLzs3ZmI0
+
+نتمني لكم التوفيق والنجاح الدائم 🌟
+        """
+    )
+
+bot.polling(none_stop=True)
